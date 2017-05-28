25fa18e99d62b57cb12d0b202406d3f70a5aa8a2
Marty Andrews <marty@martyandrews.net>
2017-05-29 00:50:21 +1000

    Recent events, player cleanup

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -5,4 +5,18 @@ class Edition < ApplicationRecord
   def name
     "#{year} #{tournament.name}"
   end
+
+  def winner
+    sorted_placings[0].player
+  end
+
+  def runner_up
+    sorted_placings[1].player
+  end
+
+  private
+
+  def sorted_placings
+    placings.sort_by{|each| each.position}
+  end
 end
